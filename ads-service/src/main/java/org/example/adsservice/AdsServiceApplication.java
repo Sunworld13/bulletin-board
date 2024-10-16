2e151d213fbfa520397597cdceb980cb0381b0c9
ilnur-yanglyaev <ilnur.yanglyaev@mail.ru>
2024-10-16 10:21:24 +0300

    second-group-services: добавлены каркасы сервисов второй группы

---
+++ ads-service/src/main/java/org/example/adsservice/AdsServiceApplication.java
@@ -0,0 +1,13 @@
+package org.example.adsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdsServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdsServiceApplication.class, args);
+    }
+
+}
